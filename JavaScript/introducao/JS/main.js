a3993e9b6716693c2e0c2297a18d1635be0aec24
lucasDEV20 <lucasgoias11@gmail.com>
2022-01-29 14:26:11 -0300

    remanejando projetos

---
+++ JavaScript/introducao/JS/main.js
@@ -0,0 +1,37 @@
+//variavel 
+//--------------------------------
+//var nome = "Lucas Araujo";
+//var idade = 22;
+//var idade2 = 10;
+//var n1 = 10;
+//var n2 = 202
+//var frase = "japao e o melhor time do mundo"
+var lista = ["maça", "pera", "laramja"]
+//--------------------------------
+
+//Alert
+//--------------------------------
+//alert(idade + idade2);
+//alert(nome + "tem " + idade + " anos");
+//alert(frase.replace("japao", "brasil"))
+//--------------------------------
+
+//console.log 
+//--------------------------------
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(idade * idade2);
+//console.log(frase.replace("japao", "Brasil"));
+//console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());
+//para ver o tamanho de uma lista utiliar length
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.push());
+//console.log(lista[0]);
+//console.log(lista.toString([0])); 
+//perde a referencia de array e ja toma forma de String
+console.log(lista());
+console.log(lista.toString());
+//--------------------------------
+lista.push("uva"); //adiiona elemtnos a lista ()
